4b0d0d14a344da86172909195129a77ebbc52cdc
SoItGoes33 <smf341@nyu.edu>
2015-09-14 20:57:06 -0700

    exercises

--- exercise-1/script.js
+++ exercise-1/script.js
@@ -1,6 +1,15 @@
 jQuery(function(){
 
-  // Your javascript code here
+  $("article").hide();
+$("#home").show();
+$("nav a").click(function() {
+   var x = $(this).attr("href");
+    $("article").hide();
+    $(x).show();
+    $("nav a").css("background-color", "#FAFAFA");
+$(this).css("background-color", "yellow");
+
+});
 
 });
 
